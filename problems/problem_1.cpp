e8deb771186ac5933afe3ad234f8d4290630406b
Asadbek-1509 <147585650+Asadbek-1509@users.noreply.github.com>
2023-11-14 18:21:50 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,13 +1,4 @@
-void problemSolution1(float consumed_water) {
-    float cost;
-    // write your code here
 #include <algorithm>
-
-//Problem 1
-//A water supply company charges the water consumption, as follows: a Fixed amount of $13.
-//b. For the first 30 cubic metres, $0.4/m3.
-//c. For the next 20 cubic metres, $0.12/m3.
-//d. For the next 10 cubic metres, $1.4/m3. e. For every additional metre, $1.5/m3.
 float problemSolution1(float consumed_water) {
     float cost = 0;
     cost = cost + 13;
@@ -36,5 +27,5 @@ float problemSolution1(float consumed_water) {
     return cost;
 }
 
-    return cost;
-}
+   
+
