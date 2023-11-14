2260b8680b9bd77636d46a6cc13005d82743b776
Asadbek-1509 <147585650+Asadbek-1509@users.noreply.github.com>
2023-11-14 19:09:49 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,36 +2,37 @@
 using namespace std;
 
 int main() {
-    // Variables to store sex and height
-    char sex;
+
+    char  sex;
     double height;
 
-    // User input for sex and height
-    cout << "Enter the person's sex (M/F): ";
+
+    cout << " ";
     cin >> sex;
-    cout << "Enter the person's height in meters: ";
+    cout << " ";
     cin >> height;
 
-    // Determine the description based on sex and height
     if (sex == 'M') {
         if (height < 1.70) {
             cout << "Short" << endl;
         } else if (height >= 1.70 && height < 1.85) {
             cout << "Normal" << endl;
-        } else {
+        } else if ( height >= 1.85) {
             cout << "Tall" << endl;
         }
-    } else if (sex == 'F') {
+    }
+
+    else if (sex == 'F') {
         if (height < 1.60) {
             cout << "Short" << endl;
         } else if (height >= 1.60 && height < 1.75) {
             cout << "Normal" << endl;
-        } else {
+        } else if ( height >= 1.75 ){
             cout << "Tall" << endl;
         }
-    } else {
-        cout << "Invalid input for sex. Please enter M or F." << endl;
     }
+
+
 
     return 0;
 }
