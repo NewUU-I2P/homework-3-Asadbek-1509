17c781ed19cdb8af0b510a27a9176c164351ba54
Asadbek-1509 <147585650+Asadbek-1509@users.noreply.github.com>
2023-11-14 18:26:57 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,8 +1,37 @@
-#include <string>
+#include <iostream>
+using namespace std;
 
-std::string problemSolution3(float height, char S) {
-    // write your code here
+int main() {
+    // Variables to store sex and height
+    char sex;
+    double height;
 
-    // use return to return your result
-    // make use of control flow statements
+    // User input for sex and height
+    cout << "Enter the person's sex (M/F): ";
+    cin >> sex;
+    cout << "Enter the person's height in meters: ";
+    cin >> height;
+
+    // Determine the description based on sex and height
+    if (sex == 'M') {
+        if (height < 1.70) {
+            cout << "Short" << endl;
+        } else if (height >= 1.70 && height < 1.85) {
+            cout << "Normal" << endl;
+        } else {
+            cout << "Tall" << endl;
+        }
+    } else if (sex == 'F') {
+        if (height < 1.60) {
+            cout << "Short" << endl;
+        } else if (height >= 1.60 && height < 1.75) {
+            cout << "Normal" << endl;
+        } else {
+            cout << "Tall" << endl;
+        }
+    } else {
+        cout << "Invalid input for sex. Please enter M or F." << endl;
+    }
+
+    return 0;
 }
